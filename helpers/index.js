f16d22bc981321ad748b0eb44268e742836ebf3a
Stepan Skichko <st.skichko@gmail.com>
2023-07-01 10:40:13 +0300

    add helpers & update contacts.js in Model folder

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,13 @@
 const HttpError = require('./HttpError');
+const handleMongooseError = require('./handleMongooseError');
+const isValidId = require('./isValidId');
+const validateBody = require('./validateBody');
+const controllerWrapper = require('./controllerWrapper');
 
 module.exports = {
   HttpError,
+  handleMongooseError,
+  isValidId,
+  validateBody,
+  controllerWrapper,
 };
--- models/contacts.js
+++ models/contacts.js
@@ -1,80 +1,131 @@
-const fs = require('fs/promises');
-const path = require('path');
-const contactsPath = path.join(__dirname, 'contacts.json');
-const { nanoid } = require('nanoid');
+// const fs = require('fs/promises');
+// const path = require('path');
+// const contactsPath = path.join(__dirname, 'contacts.json');
+// const { nanoid } = require('nanoid');
 
-async function listContacts() {
-  try {
-    const contacts = await fs.readFile(contactsPath);
-    return JSON.parse(contacts);
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Повертає масив контактів.
-}
+// async function listContacts() {
+//   try {
+//     const contacts = await fs.readFile(contactsPath);
+//     return JSON.parse(contacts);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+//   // Повертає масив контактів.
+// }
 
-async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-    const result = contacts.find(item => item.id === contactId);
-    return result || null;
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+// async function getContactById(contactId) {
+//   try {
+//     const contacts = await listContacts();
+//     const result = contacts.find(item => item.id === contactId);
+//     return result || null;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+//   // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+// }
 
-async function removeContact(contactId) {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex(item => item.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+// async function removeContact(contactId) {
+//   try {
+//     const contacts = await listContacts();
+//     const index = contacts.findIndex(item => item.id === contactId);
+//     if (index === -1) {
+//       return null;
+//     }
+//     const [result] = contacts.splice(index, 1);
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//     return result;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+//   // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+// }
 
-async function addContact(data) {
-  try {
-    const contacts = await listContacts();
-    const newContact = {
-      id: nanoid(),
-      ...data,
-    };
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Повертає об'єкт доданого контакту.
-}
+// async function addContact(data) {
+//   try {
+//     const contacts = await listContacts();
+//     const newContact = {
+//       id: nanoid(),
+//       ...data,
+//     };
+//     contacts.push(newContact);
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//     return newContact;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+//   // Повертає об'єкт доданого контакту.
+// }
 
-async function updateContact(id, data) {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex(item => item.id === id);
-    if (index === -1) {
-      return null;
-    }
-    contacts[index] = { id, ...data };
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contacts[index];
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+// async function updateContact(id, data) {
+//   try {
+//     const contacts = await listContacts();
+//     const index = contacts.findIndex(item => item.id === id);
+//     if (index === -1) {
+//       return null;
+//     }
+//     contacts[index] = { id, ...data };
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//     return contacts[index];
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      default: false,
+    },
+    favorite: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post('save', handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model('contact', contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
+  schemas,
 };
