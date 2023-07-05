227a5bb5215930907a9bee0b4f2e0741ed68df8d
Stepan Skichko <st.skichko@gmail.com>
2023-07-05 13:50:22 +0300

    add userSchema

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Set password for user'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ['starter', 'pro', 'business'],
+    default: 'starter',
+  },
+  token: String,
+});
+
+userSchema.post('save', handleMongooseError);
