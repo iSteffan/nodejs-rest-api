db8e3631f1a7a94443396585edbf2ee9773d9987
Stepan Skichko <st.skichko@gmail.com>
2023-07-06 14:14:13 +0300

    add contact owner

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,10 @@ const { Contact, addSchema, updateFavoriteSchema } = require('../models/contact'
 const { HttpError, controllerWrapper } = require('../helpers');
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, '', { skip, limit }).populate('owner', 'name email');
   res.json(result);
 };
 
@@ -20,7 +23,9 @@ const addContact = async (req, res) => {
   if (error) {
     throw HttpError(400, 'missing required name field');
   }
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
