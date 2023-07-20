f23f442ba6464e9251a7a5789f8b1b3c95649d6d
Stepan Skichko <st.skichko@gmail.com>
2023-07-20 13:26:30 +0300

    delete trash & add email verification to login controller

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -4,9 +4,6 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: '' });
 
-  // res.json({
-  //   message: 'Logout success',
-  // });
   res.status(204).json();
 };
 
--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -13,20 +13,6 @@ const nodemailerConfig = {
   },
 };
 
-// const transport = nodemailer.createTransport(nodemailerConfig);
-
-// const email = {
-//   to: 'st.skichko@gmail.com',
-//   from: 'st.skichko@meta.ua',
-//   subject: 'Test email',
-//   html: '<p><strong>Test email</strong> from localhost:3000</p>',
-// };
-
-// transport
-//   .sendMail(email)
-//   .then(() => console.log('Email send success'))
-//   .catch(error => console.log(error.message));
-
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async data => {
